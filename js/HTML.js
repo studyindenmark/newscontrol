edcb12693a644d06a1a566af5bd44047c9a39e2d
kallux <github@kallux.com>
2013-04-29 22:22:21 +0200

    Added createTag function to HTML.js

--- js/HTML.js
+++ js/HTML.js
@@ -2,9 +2,18 @@ var HTML = {
     
     createFeed: function(data) {
         var $clone = $('.template.feed').clone(true);
+        $clone.removeClass('template');
         $clone.find('.title').text(data.title);
         $clone.find('.url').attr('href', data.url).text(data.url);
         return $clone;
     },
+
+    createTag: function(data) {
+        var $clone = $('.template.tag').clone(true);
+        $clone.removeClass('template');
+        $clone.find('.title').text(data.title);
+        $clone.find('.badge').text(data.entry_count);
+        return $clone;
+    }
     
 };
