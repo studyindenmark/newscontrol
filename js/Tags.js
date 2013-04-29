66c4ad8baf1e39635cfb5fef62d1e7745bdd99da
kallux <github@kallux.com>
2013-04-29 22:23:13 +0200

    Added loadTags and fixed formatting to Tags.js

--- js/Tags.js
+++ js/Tags.js
@@ -2,26 +2,40 @@ function Tags() {
     var self = this;
 
 	self.initialized = false;
+    self.$view = $("#tags");
+    self.$ul = self.$view.find('> ul');
 
     self.init = function() {
-        self.initialized = true;
-
         $('#tags .btn.add').click(function() {
-        var $title = $('#tags .add input[name=title]'),
-            title = $title.val();
-        
-        if (!title) {
-            alert('A name needs to be provided');
-            return;
-        }
-        
-        var params = {
-            title: title
-        };
+            var $title = $('#tags .add input[name=title]'),
+                title = $title.val();
+            
+            if (!title) {
+                alert('A name needs to be provided');
+                return;
+            }
+            
+            var params = {
+                title: title
+            };
 
-        $.post('/tags', params).success(function(data) {
-            console.log('tag added', data);
+            $.post('/tags', params).success(function(data) {
+                console.log('tag added', data);
+            });
         });
-    });
+
+        self.initialized = true;
+        self.loadTags();
     }
+
+    self.loadTags = function() {
+        $.getJSON('/tags').success(function(data) {
+            console.log(data);
+            
+            $.each(data, function(i, item) {
+                var $view = HTML.createTag(item);
+                self.$ul.append($view);
+            });
+        });
+    };
 }
