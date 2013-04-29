0ea55614a676236d92d8f22f9de48d227440dfab
Per Thulin <per@youtify.com>
2013-04-29 19:33:39 +0200

    Initial work on adding of feeds

--- js/Feeds.js
+++ js/Feeds.js
@@ -5,5 +5,23 @@ function Feeds() {
 
     self.init = function() {
         self.initialized = true;
-    }
+    };
+    
+    $('#feeds .btn.add').click(function() {
+        var $url = $('#feeds .add input[name=url]');
+        var url = $url.val();
+        
+        if (!url) {
+            alert('A URL needs to be provided');
+            return;
+        }
+        
+        var params = {
+            url: url
+        };
+
+        $.post('/feeds', params).success(function(data) {
+            console.log('feed added', data);
+        });
+    });
 }
