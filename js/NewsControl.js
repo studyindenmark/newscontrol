ee3b4b1c7fa04d5fc744e07df1ef2ab502257ccb
kallux <github@kallux.com>
2013-04-29 16:26:03 +0200

    Added login modal and error modal

--- js/NewsControl.js
+++ js/NewsControl.js
@@ -1,26 +1,42 @@
 function NewsControl() {
-	var self = this;
+    var self = this;
 
-	self.init = function() {
-		// Make sure we can log without breaking old browsers
-		if (typeof(window.console) === 'undefined') {
-			window.console = { 
-				log: function() {}, 
-				error: function(exception) { alert(exception); }
-			};
-		}
-		
-		// Handle unsuccessful requests
-		$(document).ajaxError(function (e, r, ajaxOptions, thrownError) {
-    		switch (r.status) {
-    			case 403:
-    				self.showLoginModal();
-    				break;
-				case 500:
-					self.showError(r.responseText);
-					break;
-	    	}
-		});
-	};
+    self.init = function() {
+        // Make sure we can log without breaking old browsers
+        if (typeof(window.console) === 'undefined') {
+            window.console = { 
+                log: function() {}, 
+                error: function(exception) { alert(exception); }
+            };
+        }
+        
+        // Handle unsuccessful requests
+        $(document).ajaxError(function (e, r, ajaxOptions, thrownError) {
+            switch (r.status) {
+                case 403:
+                    self.showLoginModal();
+                    break;
+                case 500:
+                    self.showErrorModal(r.responseText);
+                    break;
+            }
+        });
+    };
 
+    self.showLoginModal = function() {
+        $('#loginModal').modal({
+            backdrop: 'static',
+            keyboard: true,
+            show: true
+        });
+    }
+
+    self.showErrorModal = function(message) {
+        $('#errorModal .message').text(message);
+        $('#errorModal').modal({
+            backdrop: 'static',
+            keyboard: true,
+            show: true
+        });
+    }
 }
