a76c2a3ebf7394d717299e1799be83814d54c1f0
Per Thulin <per@youtify.com>
2013-04-29 16:18:17 +0200

    Add get_current_user_model

---
+++ utils.py
@@ -0,0 +1,8 @@
+from google.appengine.api import users
+from model import User
+
+def get_current_user_model():
+    return get_youtify_user_model_for(users.get_current_user())
+
+def get_user_model_for(google_user=None):
+    return User.all().filter('google_user =', google_user).get()
