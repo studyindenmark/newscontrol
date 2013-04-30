37aaf78e8f1893f574648bb8ae9d0d864d46e20b
Per Thulin <per@youtify.com>
2013-04-30 09:59:51 +0200

    Add cron job to fetch new entries

---
+++ cron.py
@@ -0,0 +1,32 @@
+import webapp2
+import json
+import urllib
+import datetime
+
+import feedparser
+import utils
+
+from google.appengine.ext.webapp import util
+from model import InputFeed
+from model import Entry
+
+class FetchFeedsCronJob(webapp2.RequestHandler):
+    def get(self):
+        """Loop through all feeds and fetches their entries"""
+        feeds = InputFeed.all()
+            
+        self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        
+        count = 0
+        for feed in feeds:
+            count += 1
+            feed.fetch_entries()
+        
+        self.response.out.write(json.dumps({
+            'nr_of_feeds_fetched': count,
+        }))
+
+app = webapp2.WSGIApplication([
+    ('/cron/fetch_feeds', FetchFeedsCronJob),
+], debug=True)
