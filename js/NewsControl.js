9ee452013d2355a96361f9971a3ea104b2573de5
kallux <github@kallux.com>
2013-04-30 11:50:34 +0200

    Added information modal and attatched messages to status codes

--- js/NewsControl.js
+++ js/NewsControl.js
@@ -17,6 +17,12 @@ function NewsControl() {
                     // Logged out
                     self.showLoginModal();
                     break;
+                case 404:
+                    self.showInformationModal('A resource cannot be found (' + ajaxOptions.url + ')');
+                    break;
+                case 409:
+                    self.showInformationModal('The item is not unique.');
+                    break;
                 case 500:
                     // Server error
                     self.showErrorModal(r.responseText);
@@ -43,6 +49,15 @@ function NewsControl() {
     self.showErrorModal = function(message) {
         $('#errorModal .message').html(message);
         $('#errorModal').modal({
+            backdrop: 'static',
+            keyboard: true,
+            show: true
+        });
+    };
+
+    self.showInformationModal = function(message) {
+        $('#infoModal .message').html(message);
+        $('#infoModal').modal({
             backdrop: 'static',
             keyboard: true,
             show: true
