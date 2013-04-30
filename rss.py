1ba90f53d315df9416478b2930f562da27abdca1
kallux <github@kallux.com>
2013-04-30 14:39:30 +0200

    Added rss.py and template.rss for rendering the RSS feed

---
+++ rss.py
@@ -0,0 +1,44 @@
+import os
+import json
+import webapp2
+import json
+import urllib
+
+import utils
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+from model import Tag
+from model import Entry
+
+import logging
+
+class RSSHandler(webapp2.RequestHandler):
+    def get(self, user, tag_title):
+        """Gets RSS feed for a user, filtered by tag"""
+        user = utils.get_user_model_by_id_or_nick(user)
+        
+        if not user:
+            self.error(403)
+            return
+            
+        tag = Tag.all().ancestor(user.key()).filter('title_lower =', tag_title.lower()).get()
+
+        if not tag:
+            self.error(404)
+            return
+
+        entries = Entry.all().filter('tags =', tag.key()).filter('published =', True).fetch(20)
+        entries = [e.to_struct() for e in entries]
+            
+        path = os.path.join(os.path.dirname(__file__), 'template.rss')
+        self.response.headers['Content-Type'] = 'application/xml; charset=utf-8'
+        self.response.out.write(template.render(path, {
+            'entries': entries,
+            'url': self.request.url,
+            'title': tag_title,
+        }))
+
+app = webapp2.WSGIApplication([
+    ('/(.*)/(.*)\.rss', RSSHandler),
+], debug=True)
