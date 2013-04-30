0c9ee2750bd5c0f36324593a77ee45488fe6c526
Per Thulin <per@youtify.com>
2013-04-30 10:23:05 +0200

    Load feed entries

--- js/HTML.js
+++ js/HTML.js
@@ -7,6 +7,14 @@ var HTML = {
         $clone.find('.url').attr('href', data.url).text(data.url);
         return $clone;
     },
+    
+    createEntry: function(data) {
+        var $clone = $('.template.news').clone(true);
+        $clone.removeClass('template');
+        $clone.find('.title').text(data.title);
+        $clone.find('.url').attr('href', data.url).text(data.url);
+        return $clone;
+    },
 
     createTag: function(data) {
         var $clone = $('.template.tag').clone(true);
--- js/News.js
+++ js/News.js
@@ -2,8 +2,21 @@ function News() {
     var self = this;
     
     self.initialized = false;
+    self.$view = $("#news");
+    self.$ul = self.$view.find('> ul');
 
     self.init = function() {
         self.initialized = true;
-    }
+        self.loadFeeds();
+    };
+    
+    self.loadFeeds = function() {
+        $.getJSON('/news').success(function(data) {
+            console.log('news loaded', data);
+            $.each(data, function(i, item) {
+                var $view = HTML.createEntry(item);
+                self.$ul.append($view);
+            });
+        });
+    };
 }
