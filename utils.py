886aceda76db8fb6f8313e9736cb2b7b5e4343dd
kallux <github@kallux.com>
2013-04-30 14:37:15 +0200

    Added get_user_model_by_id_or_nick

--- utils.py
+++ utils.py
@@ -6,3 +6,9 @@ def get_current_user_model():
 
 def get_user_model_for(google_user=None):
     return User.all().filter('google_user =', google_user).get()
+
+def get_user_model_by_id_or_nick(id_or_nick):
+    if id_or_nick.isdigit():
+        return User.get_by_id(int(id_or_nick))
+    else:
+        return User.all().filter('nickname_lower = ', id_or_nick.lower()).get()
