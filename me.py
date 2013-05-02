438e790cdfa6692f57f161d97e7343da3523b847
kallux <github@kallux.com>
2013-05-02 21:44:38 +0200

    Fixed bug where uninvited users caused a crash

--- me.py
+++ me.py
@@ -23,6 +23,9 @@ class MeHandler(webapp2.RequestHandler):
             
             if users.is_current_user_admin():
                 user = utils.create_user(google_user)
+            else:
+                self.error(401)
+                return
         
         self.response.headers['Content-Type'] = 'application/json; charset=utf-8'
         self.response.headers['Access-Control-Allow-Origin'] = '*'
