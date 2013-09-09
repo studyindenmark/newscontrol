studyindenmark/newscontrol

commit aee5de6207d70a9895117353cfe2566743fddc0f

Make sure that there are no entries with the same guid and ancestor in db on fetch

model.py
@@ -70,16 +70,18 @@ class InputFeed(db.Model):
                 content = entry.get('summary')
 
             if fetch_all or (published > self.time_fetched):
-                m = Entry(
-                    guid=entry.get('guid'),
-                    link=entry.get('link'),
-                    parent=self,
-                    user=self.parent(),
-                    content=content,
-                    title=entry.get('title'),
-                    time_published=published,
-                )
-                m.put()
+                existing_entry = Entry.all().ancestor(self).filter('guid =', entry.get('guid')).count(read_policy=EVENTUAL_CONSISTENCY, limit=1) 
+                if not existing_entry:
+                    m = Entry(
+                        guid=entry.get('guid'),
+                        link=entry.get('link'),
+                        parent=self,
+                        user=self.parent(),
+                        content=content,
+                        title=entry.get('title'),
+                        time_published=published,
+                    )
+                    m.put()
             
         self.time_fetched = datetime.datetime.utcnow()
         self.save()

commit b5876644c7e699c20bf94d3ad9586337c9e45932

Fix crash when fetching feed, introduced in aee5de6207d70a9895117353cfe2566743fddc0f

model.py
@@ -70,7 +70,7 @@ class InputFeed(db.Model):
                 content = entry.get('summary')
 
             if fetch_all or (published > self.time_fetched):
-                existing_entry = Entry.all().ancestor(self).filter('guid =', entry.get('guid')).count(read_policy=EVENTUAL_CONSISTENCY, limit=1) 
+                existing_entry = Entry.all().ancestor(self).filter('guid =', entry.get('guid')).count(read_policy=db.EVENTUAL_CONSISTENCY, limit=1) 
                 if not existing_entry:
                     m = Entry(
                         guid=entry.get('guid'),